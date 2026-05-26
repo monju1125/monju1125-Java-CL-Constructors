5da47dd9afe6cfdf2169abc3ff57b01e89fa7457
monju1125 <monju1125@github.com>
2026-05-26 18:20:36 +0000

    Test run: 0/3 passed, 3 failed, 0 skipped - 2026-05-26T18:20:36.620Z

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -31,7 +31,7 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
-        this.level = 1;
+        this.playerName = playerName;
 
     }
 
